90adbd95f2dd363a2fb35c06405d95347d473e3e
Dave Lucia <dlucia@bloomberg.net>
2016-08-05 07:57:30 -0400

    Start upgrading to elm 0.17

--- Pong.elm
+++ Pong.elm
@@ -1,14 +1,14 @@
 -- See this document for more information on making Pong:
 -- http://elm-lang.org/blog/Pong.elm
-import Color (..)
-import Graphics.Collage (..)
-import Graphics.Element (..)
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
 import Keyboard
 import Signal
-import Signal (..)
+import Signal exposing (..)
 import Text
 import Char
-import Time (..)
+import Time exposing (..)
 import Window
 
 -- SIGNALS
